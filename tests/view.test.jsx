936437e6848a23264a28dce68ba77ad20e7663f7
ericnem <ericnem@outlook.com>
2023-06-30 15:25:56 -0400

    Added tests for View Page

--- tests/view.test.jsx
+++ tests/view.test.jsx
@@ -2,9 +2,24 @@ import {render, screen, fireEvent} from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
 import ViewPage from "../src/pages/ViewPage";
 import {BrowserRouter as Router} from 'react-router-dom';
+import userEvent from '@testing-library/user-event'
+
 
 describe ("View page test", () => {
-  test('Click My Gallery Button', () => {
+  test('If left arrow is initially disabled', () => {
+
+
+    render(
+      <Router>
+        <ViewPage/>
+      </Router>
+    );
+
+    const buttons = screen.queryAllByRole('button')
+    expect(buttons[4]).toBeUndefined()
+  });
+
+  test('If gallery button navigates to gallery', () => {
 
     render(
       <Router>
