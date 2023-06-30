d117f853a74605b468aba82c20a0ae324c2900d3
ericnem <ericnem@outlook.com>
2023-06-30 12:24:22 -0400

    Configured test framework

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:8081'
