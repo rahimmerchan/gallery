474b9be818922701527235077763934a21ac795e
bryanjiang117 <haozhoujiang@gmail.com>
2023-07-02 20:55:04 -0400

    Very Minor Stuff

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import ViewPage from '/src/pages/ViewPage.jsx';
-import Menu from '/src/pages/Menu.jsx';
-import Login from '/src/components/login.jsx';
-import SignUpPage from '/src/pages/SignUpPage.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ViewPage from "/src/pages/ViewPage.jsx";
+import Menu from "/src/pages/Menu.jsx";
+import Login from "/src/components/login.jsx";
+import SignUpPage from "/src/pages/SignUpPage.jsx";
 
 let user = "alice@gmail.com";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
   },
   {
     path: "/login",
-    element: <Login/>
+    element: <Login />,
   },
   {
     path: "/signUp",
-    element: <SignUpPage/>
+    element: <SignUpPage />,
   },
   {
     path: "/menu",
-    element: <Menu user={user}/>
+    element: <Menu user={user} />,
   },
   {
     path: "/viewpage",
-    element: <ViewPage user={user}/>
+    element: <ViewPage user={user} />,
   },
 ]);
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
